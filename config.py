60b4c3156b799627c16ee78b8b3cd8a64f2d59ad
Noctious1 <47297638+Noctious1@users.noreply.github.com>
2020-02-05 01:09:27 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+TOKEN = 'NjczNjQ2MDg0MTM1ODQ1ODg4.XjdD1w.DwxMMBdEO-CDfrcpzOYzFWEp5Ck' #bot token
+
+POST_ID = 584030249675653151
+
+ROLES = {
+    '🎮': 584054628681908229, #gamer role
+    '🤯': 673201275898298370, #dolboeb role
+    '⛏': 674319826751193098, #minecraft role
+    '🔥': 674319712108281900, #anime role
+    '💣': 674318934622732298, #bomb role
+    '🔫': 674319381836070913, #csgo role
+    '💨': 674318817907703876, #pyl role
+}
+
+EXCROLES = ()
+
+MAX_ROLES_PER_USER = 20
