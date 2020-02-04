26fe5e6c06c5867068f8d3a58aa605024549b179
Noctious1 <47297638+Noctious1@users.noreply.github.com>
2020-02-05 01:39:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,22 @@ from discord import utils
 
 import config
 
+POST_ID = 584030249675653151
+
+ROLES = {
+    '🎮': 584054628681908229, #gamer role
+    '🤯': 673201275898298370, #dolboeb role
+    '⛏': 674319826751193098, #minecraft role
+    '🔥': 674319712108281900, #anime role
+    '💣': 674318934622732298, #bomb role
+    '🔫': 674319381836070913, #csgo role
+    '💨': 674318817907703876, #pyl role
+}
+
+EXCROLES = ()
+
+MAX_ROLES_PER_USER = 20
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -52,4 +68,4 @@ class MyClient(discord.Client):
 
 # RUN
 client = MyClient()
-client.run(config.TOKEN)
+client.run('NjczNjQ2MDg0MTM1ODQ1ODg4.XjdD1w.DwxMMBdEO-CDfrcpzOYzFWEp5Ck')
